636237c8b936dcca2468d6a27231135041e309b0
hoanglv03 <luonghoang29022003@gmail.com>
2023-07-19 16:28:44 +0700

    orboadScreen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
@@ -24,37 +18,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
+import Screen1 from './src/view/orboardscreen/Screen1';
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -63,59 +27,10 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <View>
-        <Text>Done</Text>
-      </View>
-      {/* <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView> */}
-    </SafeAreaView>
+    <Screen1/>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
--- src/view/orboardscreen/Screen1.js
+++ src/view/orboardscreen/Screen1.js
@@ -1,12 +1,191 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import PagerView from 'react-native-pager-view';
 
 export default function Screen1() {
+  const ref = useRef();
+  const pinkColor = '#F582AE';
+  const blueColor = '#001858';
+  const [pageSelect, setPageSelect] = useState('');
   return (
-    <View>
-      <Text>Screen1</Text>
+    <View style={{flex: 1, backgroundColor: '#FEF6E4'}}>
+      <PagerView ref={ref} style={styles.viewPager} initialPage={0}>
+        <View style={styles.page} key="1">
+          <Image
+            style={{position: 'absolute', left: 0}}
+            source={require('../../assets/image/weel.png')}
+          />
+          <Image
+            style={{position: 'absolute', right: 0,top:0}}
+            source={require('../../assets/image/weel-red-1.png')}
+          />
+          <Image source={require('../../assets/image/imagescreen1.png')} />
+          <Text style={styles.title}>
+            Chào mừng bạn đến với thế giới dành cho{' '}
+            <Text style={{color: pinkColor}}>thú cưng</Text>
+          </Text>
+          <View style={styles.buttonView}>
+            <Pressable
+              onPress={() => ref.current?.setPage(1)}
+              style={[
+                styles.button,
+                {
+                  backgroundColor: pinkColor,
+                  borderRadius: 3,
+                  width: 185,
+                  height: 52,
+                },
+              ]}>
+              <Text
+                style={{
+                  color: blueColor,
+                  fontSize: 20,
+                }}>
+                Tiếp tục
+              </Text>
+              <Image
+                style={{position: 'absolute', right: 0}}
+                source={require('../../assets/image/icon_paw.png')}
+              />
+            </Pressable>
+          </View>
+        </View>
+        <View style={[styles.page]} key="2">
+        <Image
+            style={{position: 'absolute', left: 0,top:0}}
+            source={require('../../assets/image/weel-red-2.png')}
+          />
+           <Image
+            style={{position: 'absolute', bottom:-50,left:20}}
+            source={require('../../assets/image/pone.png')}
+          />
+          <Image
+            style={{position: 'absolute', right:0,bottom:140}}
+            source={require('../../assets/image/weel-1.png')}
+          />
+          <Image source={require('../../assets/image/imagescreen2.png')} />
+          <Text style={styles.title}>
+            Khám phá nguồn cung cấp đồ pet
+            <Text style={{color: pinkColor}}> đa dạng </Text>
+            và<Text style={{color: pinkColor}}> tiện lợi </Text>
+            được giao đến<Text style={{color: pinkColor}}> tận nhà! </Text>
+          </Text>
+
+          <View style={styles.buttonView}>
+            <Pressable
+              onPress={() => ref.current?.setPage(0)}
+              style={[
+                styles.button,
+                {backgroundColor: '#E3D7D7', borderRadius: 3, marginRight: 8},
+              ]}>
+              <Text style={{fontSize: 18, color: blueColor}}>Quay lại</Text>
+            </Pressable>
+            <Pressable
+              onPress={() => ref.current?.setPage(2)}
+              style={[
+                styles.button,
+                {backgroundColor: pinkColor, borderRadius: 3, marginLeft: 8},
+              ]}>
+              <Text
+                style={[
+                  styles.textButton,
+                  {
+                    fontSize: 18,
+                    color: blueColor,
+                  },
+                ]}>
+                Tiếp Tục
+              </Text>
+              <Image
+                style={{position: 'absolute', right: 0, top: -2}}
+                source={require('../../assets/image/icon_paw.png')}
+              />
+            </Pressable>
+          </View>
+        </View>
+        <View style={styles.page} key="3">
+        <Image
+            style={{position: 'absolute', left:0,bottom:140}}
+            source={require('../../assets/image/weel-2.png')}
+          />
+          <Image
+            style={{position: 'absolute', right:-100,top:0}}
+            source={require('../../assets/image/pone.png')}
+          />
+          <Image
+            style={{position: 'absolute', right:0,bottom:0}}
+            source={require('../../assets/image/weel-red-3.png')}
+          />
+          <Image source={require('../../assets/image/imagescreen3.png')} />
+          <Text style={styles.title}>
+            Hãy bắt đầu chúng tôi sẽ cho bạn thấy một{' '}
+            <Text style={{color: pinkColor}}>hệ sinh thái</Text> dành cho thú
+            cưng của bạn
+          </Text>
+
+          <View style={styles.buttonView}>
+            <Pressable
+              onPress={() => ref.current?.setPage(1)}
+              style={[
+                styles.button,
+                {
+                  backgroundColor: pinkColor,
+                  borderRadius: 3,
+                  width: 185,
+                  height: 52,
+                },
+              ]}>
+              <Text
+                style={{
+                  color: blueColor,
+                  fontSize: 20,
+                }}>
+                Tiếp tục
+              </Text>
+              <Image
+                style={{position: 'absolute', right: 0}}
+                source={require('../../assets/image/icon_paw.png')}
+              />
+            </Pressable>
+          </View>
+        </View>
+      </PagerView>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewPager: {
+    flex: 1,
+  },
+  page: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 70,
+  },
+  title: {
+    color: '#001858',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 70,
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  content: {
+    marginTop: 10,
+
+    color: '#9F9F9F',
+  },
+  buttonView: {
+    flexDirection: 'row',
+    marginLeft: 45,
+    marginRight: 45,
+  },
+  button: {
+    width: '50%',
+    height: 50,
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
