e6d051b1ffc9926a7f8f924341a05eb8d88a91ce
Unmeinh <khongphaihoanglinh@gmail.com>
2023-07-21 18:12:55 +0700

    Update 21/07/2023

---
+++ src/styles/temp.style.js
@@ -0,0 +1,14 @@
+import { StyleSheet, StatusBar, Dimensions } from 'react-native';
+
+let WindowWidth = Dimensions.get("window").width;
+let WindowHeight = Dimensions.get("window").height;
+let StatusHeight = StatusBar.currentHeight;
+let yellowWhite = '#FEF6E4';
+let lightBrown = '#F3D2C1';
+let lighBlue = '#8BD3DD';
+let pinkLotus = '#F582AE';
+let darkBlue = '#001858';
+
+export default StyleSheet.create({
+    
+});
