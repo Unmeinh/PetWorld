c02206cef291ff7d7683aff93b09636007172b3e
hoanglv03 <luonghoang29022003@gmail.com>
2023-07-21 07:40:55 +0700

    set home

--- App.tsx
+++ App.tsx
@@ -1,24 +1,10 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
-import SlashScreen from './src/view/slashscreen/SlashScreen';
-import OrboadScreen from './src/view/orboardscreen/OrboadScreen';
-import LoginScreen from './src/view/form/LoginScreen';
-import RegisterScreen from './src/view/form/RegisterScreen';
-const Stack = createStackNavigator();
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import StackScreen from './src/navigation/StackScreen'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={() => ({headerShown: false})}
-        initialRouteName="SlashScreen">
-        <Stack.Screen name="SlashScreen" component={SlashScreen} />
-        <Stack.Screen name="OrboadScreen" component={OrboadScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <StackScreen/>
+  )
 }
+const styles = StyleSheet.create({})
