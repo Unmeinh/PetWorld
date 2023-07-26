05992b2845eb788e58b0cd3eba4d462b246cb390
Unmeinh <khongphaihoanglinh@gmail.com>
2023-07-26 18:28:08 +0700

    f

--- src/navigation/StackScreen.js
+++ src/navigation/StackScreen.js
@@ -5,6 +5,7 @@ import SlashScreen from '../view/slashscreen/SlashScreen';
 import OrboadScreen from '../view/orboardscreen/OrboadScreen';
 import LoginScreen from '../view/form/LoginScreen';
 import NaviTabSreen from './NaviTabSreen';
+import SearchFilters from '../view/search/SearchFilters';
 import ForgetPassword from '../view/form/ForgetPassword';
 import ConfirmOTP from '../view/form/ConfirmOTP';
 import ChangePassword from '../view/form/ChangePassword';
@@ -20,6 +21,7 @@ export default function StackScreen() {
         <Stack.Screen name="OrboadScreen" component={OrboadScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="NaviTabSreen" component={NaviTabSreen} />
+        <Stack.Screen name="SearchFilters" component={SearchFilters} />
         <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
         <Stack.Screen name="ConfirmOTP" component={ConfirmOTP} />
         <Stack.Screen name="ChangePassword" component={ChangePassword} />
