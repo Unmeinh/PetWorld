d8c4a2d01831af9e812946dd253ea9023fc823be
hoanglv03 <luonghoang29022003@gmail.com>
2023-07-29 17:23:07 +0700

    fix xuung dot

--- src/view/home/HomeScreen.js
+++ src/view/home/HomeScreen.js
@@ -15,7 +15,6 @@ import Slider from '../../component/Slider';
 import {makeMutable} from 'react-native-reanimated';
 import {useSelector} from 'react-redux';
 import CategoryList from '../../component/CategoryList';
-
 import ListProductHorizontal from '../../component/ListProductHorizotal';
 export default function HomeScreen({navigation}) {
   const [countCart, setCountCart] = useState(0);
