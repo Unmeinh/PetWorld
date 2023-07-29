df48abfa21a2aa072e6c09b71608b37890945857
hoanglv03 <luonghoang29022003@gmail.com>
2023-07-29 07:58:34 +0700

    fix xng dot

--- src/navigation/StackScreen.js
+++ src/navigation/StackScreen.js
@@ -4,10 +4,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import SlashScreen from '../view/slashscreen/SlashScreen';
 import OrboadScreen from '../view/orboardscreen/OrboadScreen';
 import LoginScreen from '../view/form/LoginScreen';
-import ForgetPassword from '../view/form/ForgetPassword';
 import NaviTabSreen from './NaviTabSreen';
 import SearchFilters from '../view/search/SearchFilters';
+
 import ListProductScreen from '../view/shopping/ListProductScreen';
+
+import ForgetPassword from '../view/form/ForgetPassword';
+import ConfirmOTP from '../view/form/ConfirmOTP';
+import ChangePassword from '../view/form/ChangePassword';
 const Stack = createStackNavigator();
 
 export default function StackScreen() {
@@ -20,9 +24,11 @@ export default function StackScreen() {
         <Stack.Screen name="OrboadScreen" component={OrboadScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="NaviTabSreen" component={NaviTabSreen} />
-        <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
         <Stack.Screen name="SearchFilters" component={SearchFilters} />
         <Stack.Screen name="ListProductScreen" component={ListProductScreen} />
+        <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
+        <Stack.Screen name="ConfirmOTP" component={ConfirmOTP} />
+        <Stack.Screen name="ChangePassword" component={ChangePassword} />
       </Stack.Navigator>
     </NavigationContainer>
   );
