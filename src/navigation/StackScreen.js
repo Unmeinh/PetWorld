8c680d4d8c9fa0051c48de16b05c4503d9b34bf2
preci0us03 <102132883+preci0us03@users.noreply.github.com>
2023-08-09 16:00:59 +0700

    update

--- src/navigation/StackScreen.js
+++ src/navigation/StackScreen.js
@@ -12,6 +12,9 @@ import ChangePassword from '../view/form/ChangePassword';
 import ListProductScreen from '../view/shopping/ListProductScreen';
 import NewPost from '../view/blog/NewBlog';
 import DetailProduct from '../view/shopping/DetailProduct'
+import NotifyScreen from '../view/notify/NotifyScreen';
+import SettingNotify from '../view/notify/SettingNotify';
+import AccountScreen from '../view/account/AccountScreen';
 const Stack = createStackNavigator();
 
 export default function StackScreen() {
@@ -30,6 +33,9 @@ export default function StackScreen() {
         <Stack.Screen name="ChangePassword" component={ChangePassword} />
         <Stack.Screen name="ListProductScreen" component={ListProductScreen} />
         <Stack.Screen name="NewPost" component={NewPost} />
+        <Stack.Screen name="SettingNotify" component={SettingNotify} />
+        <Stack.Screen name="NotifyScreen" component={NotifyScreen} />
+        <Stack.Screen name="AccountScreen" component={AccountScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
