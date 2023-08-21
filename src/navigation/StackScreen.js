b43a8ba5e6972ca45cfc09a6f1f70b8e904f3f38
FinnHoang <luonghoang29022003@gmail.com>
2023-08-21 09:42:52 +0700

    fix xung dot

--- src/navigation/StackScreen.js
+++ src/navigation/StackScreen.js
@@ -14,6 +14,11 @@ import NewPost from '../view/blog/NewBlog';
 import DetailProduct from '../view/shopping/DetailProduct';
 import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
 import ShopScreen from '../view/shopping/ShopScreen';
+import CartScreen from '../view/shopping/CartScreen';
+import SummaryBill from '../view/shopping/SummaryBill';
+import ListAddress from '../view/account/ListAddress';
+import AddNewAddress from '../view/account/AddNewAddress';
+import ChatScreen from '../view/chat/ChatScreen';
 const Stack = createSharedElementStackNavigator();
 
 export default function StackScreen() {
@@ -61,7 +66,16 @@ export default function StackScreen() {
             return [objAni('image'),objAni('name'),objAni('price'),objAni('rate')];
           }}
         />
-        
+        <Stack.Screen name="ShopScreen" component={ShopScreen} />
+        <Stack.Screen name="MyPage" component={MyPage} />
+        <Stack.Screen name="ViewPage" component={ViewPage} />
+        <Stack.Screen name="ListFollow" component={ListFollow} />
+        <Stack.Screen name="ShopScreen" component={ShopScreen} />
+        <Stack.Screen name="CartScreen" component={CartScreen} />
+        <Stack.Screen name="SummaryBill" component={SummaryBill} />
+        <Stack.Screen name="ListAddress" component={ListAddress} />
+        <Stack.Screen name="AddNewAddress" component={AddNewAddress} />
+        <Stack.Screen name="ChatScreen" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/redux/reducers/comment/ListCommentReducer.js
+++ src/redux/reducers/comment/ListCommentReducer.js
@@ -1,18 +1,18 @@
 import listComment from '../../../data/comment';
 const initState = listComment;
 const listCommentReducer = (state = initState, action) => {
-  console.log(action.payload);
-  switch (action.type) {
-      case "select/idBlog":
-          [...state]
-          if (state.filter(e => e.idBlog === action.payload) == undefined) {
-              return {};
-          } else {
-              return state.filter(e => e.idBlog === action.payload);
-          }
-      default:
-          return state;
-  }
+    console.log(action.payload);
+    switch (action.type) {
+        case "select/idBlog":
+            [...state]
+            if (state.filter(e => e.idBlog === action.payload) == undefined) {
+                return {};
+            } else {
+                return state.filter(e => e.idBlog === action.payload);
+            }
+        default:
+            return state;
+    }
 }
 
 export default listCommentReducer;
