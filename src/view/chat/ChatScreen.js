fc3c82f1f287eb77f58b9ba7bac26a200e897550
FinnHoang <luonghoang29022003@gmail.com>
2023-08-23 16:33:52 +0700

    fix back cart screen

--- src/view/chat/ChatScreen.js
+++ src/view/chat/ChatScreen.js
@@ -1,9 +1,14 @@
+import { useNavigation } from '@react-navigation/native';
 import React, {useState, useCallback, useEffect} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
 import {SafeAreaView} from 'react-native-safe-area-context';
-export default function ChatScreen() {
+import Icon from 'react-native-vector-icons/AntDesign';
+export default function ChatScreen({route}) {
+  const data = route.params.item;
+  const navigation = useNavigation();
   const [messages, setMessages] = useState([]);
-  const keyApiChatGPT = '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'
+  const keyApiChatGPT = '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';
   useEffect(() => {
     setMessages([
       {
@@ -13,11 +18,12 @@ export default function ChatScreen() {
         user: {
           _id: 2,
           name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
+          avatar:
+            'https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/08/28/15/rexfeatures_845638j.jpg?quality=75&width=1200&auto=webp',
         },
       },
     ]);
-    callApi("Hello")
+    callApi('Hello');
   }, []);
 
   const onSend = useCallback((messages = []) => {
@@ -25,7 +31,7 @@ export default function ChatScreen() {
       GiftedChat.append(previousMessages, messages),
     );
   }, []);
-  
+
   const callApi = async () => {
     const res = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
@@ -34,26 +40,59 @@ export default function ChatScreen() {
         Authorization: `Bearer ${keyApiChatGPT}`,
       },
       body: JSON.stringify({
-        "model": "text-davinci-003",
-        "prompt": "Say this is a test",
-        "max_tokens": 7,
-        "temperature": 0
-      })
+        model: 'text-davinci-003',
+        prompt: 'Say this is a test',
+        max_tokens: 7,
+        temperature: 0,
+      }),
     });
-    const data =  await res.json()
-    if(data){
-      console.log("Data",data);
+    const data = await res.json();
+    if (data) {
+      console.log('Data', data);
     }
   };
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#FEF6E4'}}>
+      <View style={[styles.flexRow,styles.header]}>
+        <Icon name="left" size={24} color='#001858' onPress={() => navigation.goBack()}/>
+        <View style={[styles.flexRow,{flexGrow:1,marginLeft:8}]}>
+          <View style={styles.flexRow}>
+            <Image source={{uri: data.avatar}} style={styles.avatar} />
+            <View style={styles.dotAvatar} />
+          </View>
+          <Text style={styles.name}>{data.name}</Text>
+        </View>
+      </View>
       <GiftedChat
         messages={messages}
         onSend={messages => onSend(messages)}
         user={{
           _id: 1,
         }}
+        alwaysShowSend={true}
+        placeholder="Aa"
+        renderAvatarOnTop={true}
       />
     </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+  name:{
+    fontFamily:'ProductSansBold',
+    fontSize:18,
+    color:'#001858',
+    marginLeft:8
+  },
+  header:{margin:10},
+  dotAvatar: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: '#3efc21',
+    position: 'absolute',
+    bottom: 2,
+    right: 2,
+  },
+  flexRow:{flexDirection:'row',alignItems:'center'},
+  avatar: {width: 40, height: 40, borderRadius:20},
+});
