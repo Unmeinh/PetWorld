242709170641e7318864cde3238735ad2dc97cd0
FinnHoang <luonghoang29022003@gmail.com>
2023-08-30 12:29:47 +0700

    fix xung dot api

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,11 +1,31 @@
 import axios from "axios";
-const api = "10.0.2.2:3000"
-const client = axios.create({
-    baseURL: `http://${api}/api`,
-    headers: {
-        "Accept":"application/json",
-        "Content-Type": "application/json"
-    }
+const axiosJSON = axios.create();
+const apiURL = "https://10.0.2.2:3000/api";
+
+axiosJSON.defaults.baseURL = apiURL;
+
+axiosJSON.defaults.headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    "Content-Type": "application/x-www-form-urlencoded",
+};
+
+const axiosFormData = axios.create();
+
+axiosFormData.defaults.baseURL = apiURL;
+
+axiosFormData.defaults.headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Content-Type': 'multipart/form-data',
+};
+
+// axiosJSON.defaults.timeout = 2000;
+
+// axiosJSON.defaults.withCredentials = true;
+const instance = axios.create({
+    baseURL: `http://10.0.2.2:3000/api`,
 });
 
-export default client;
+export {axiosJSON, axiosFormData};
+export default instance;
