aaf148e056ca0cce5642698147e26ca9c76683dd
Unmeinh <117454523+Unmeinh@users.noreply.github.com>
2023-10-03 23:40:48 +0700

    Update axios.config.js

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -3,8 +3,6 @@ import { storageMMKV } from "../storage/storageMMKV";
 
 const apiURL = "https://9307-2402-800-61c4-4085-1115-3d2b-6c5f-cb97.ngrok-free.app/api";
 
-// axiosAPi.defaults.timeout = 2000;
-
 // axiosAPi.defaults.withCredentials = true;
 let axiosAPi = axios.create();
 
